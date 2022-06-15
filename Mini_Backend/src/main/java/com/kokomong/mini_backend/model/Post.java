399668929da88ddc8e2803daae266e219ea0c314
joo-hui <02hjooo@gmail.com>
2022-06-15 15:38:58 +0900

    게시글에 닉네임 등록해서 상세보기, 메인에 출력

--- Mini_Backend/src/main/java/com/kokomong/mini_backend/model/Post.java
+++ Mini_Backend/src/main/java/com/kokomong/mini_backend/model/Post.java
@@ -2,6 +2,7 @@ package com.kokomong.mini_backend.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.kokomong.mini_backend.dto.PostRequestDto;
+import com.kokomong.mini_backend.security.UserDetailsImpl;
 import lombok.*;
 
 import javax.persistence.*;
@@ -22,25 +23,44 @@ public class Post extends Timestamped { //Entity class /
     private String username;  // user fk
 
     @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
     private String img; //이미지 이름
 
     @Column(nullable = false)
     private String text;    //내용
 
+
+
     @JsonManagedReference
     @OneToMany(mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments;
 
-
-
-
-    //게시글 생성
-    public Post(PostRequestDto requestDto) {
-        this.username = requestDto.getUsername();
-        this.img = requestDto.getImg();
-        this.text = requestDto.getText();
-
+    public Post(String username, String nickname, String img, String text) {
+        this.username = username;
+        this.nickname = nickname;
+        this.img = img;
+        this.text = text;
     }
+
+//    public Post(User user, PostRequestDto requestDto) {
+//       this.username = user.getUsername();
+//       this.nickname = user.getNickname();
+//        this.img = requestDto.getImg();
+//        this.text = requestDto.getText();
+//
+//    }
+
+
+//    //게시글 생성
+//    public Post(PostRequestDto requestDto) {
+//        this.username = requestDto.getUsername();
+//        this.img = requestDto.getImg();
+//        this.text = requestDto.getText();
+//
+//    }
+
 
     //게시글 수정
     public void updatePost(PostRequestDto requestDto) {
